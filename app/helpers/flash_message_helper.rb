065cac7e66838aafa0a8c6819def4904bb1d1ebc
Walmir Neto <owalmirneto@gmail.com>
2020-03-19 16:00:59 -0300

    Corrige onfesivas do rubocop

--- app/helpers/flash_message_helper.rb
+++ app/helpers/flash_message_helper.rb
@@ -2,14 +2,14 @@
 
 module FlashMessageHelper
   def flash_messages
-    @flash_messages ||= flashes.map do |type, message|
+    messages ||= flashes.map do |type, message|
       bootstrap_alert(type, message)
     end
 
-    safe_join(@flash_messages || [])
+    safe_join(messages || [])
   end
 
   def flashes
-    @flashes ||= flash.to_h.symbolize_keys
+    flash.to_h.symbolize_keys
   end
 end
