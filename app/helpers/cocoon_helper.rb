e21bf3b3f9df1033241e32599c00e8411e23ef70
Walmir Neto <owalmirneto@gmail.com>
2020-03-24 11:10:31 -0300

    Adiciona tradução dos botões do cocoon

--- app/helpers/cocoon_helper.rb
+++ app/helpers/cocoon_helper.rb
@@ -2,14 +2,14 @@
 
 module CocoonHelper
   def cocoon_link_to_add_association(form, association, options = {})
-    label = options[:label] || '+ adicionar'
+    label = options[:label] || t('buttons.cocoon.add')
     html_class = options[:html_class] || 'btn btn-sm btn-success'
 
     link_to_add_association(label, form, association, class: html_class)
   end
 
   def cocoon_link_to_remove_association(form, label = nil)
-    label ||= 'x excluir'
+    label ||= t('buttons.cocoon.remove')
     html_class = 'btn btn-sm btn-danger'
 
     link_to_remove_association(label, form, class: html_class, title: label)
